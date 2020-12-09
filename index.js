eb1199daae516703c420d9dda72cf9abe2184b9d
Eric Butz <ericgbutz@gmail.com>
2020-12-09 16:51:29 -0700

    Skipping Staking tests

--- index.js
+++ index.js
@@ -16,21 +16,16 @@ describe('TEST SUITE', () => {
   //require('./tests/action-whitelisting.js'); // FIP-12, fio v2.0.0, fio.contracts v2.0.0 // Causes future tests to fail. Only run alone.
   require('./tests/transfer-domain.js'); // FIP-1.a, fio v2.0.0, fio.contracts v2.1.0
   require('./tests/paging.js'); // FIP-2, fio v2.0.0, fio.contracts v2.1.0
-  //require('./tests/cancel-funds-request.js'); // FIP-3, fio v2.0.0, fio.contracts v2.1.0
   require('./tests/removeaddress.js'); // FIP-4, fio v2.0.0, fio.contracts v2.1.0
   require('./tests/txn-resubmit.js'); //Available with fiosdk_typescript v1.2.0
   require('./tests/burn-address.js'); // FIP-7
   //require('./tests/fee-voting-fee-setting.js'); // FIP-10
   //require('./tests/producer-fee-setting.js');  // FIP-10
-  require('./tests/record-obt-data.js'); //FIP-1b testing
-
+  require('./tests/record-obt-data.js'); //FIP-1.b testing
+  //require('./tests/transfer-address.js'); // FIP-1.b
   require('./tests/transfer-locked-tokens.js');  // FIP-6 locking tests
   require('./tests/transfer-locked-tokens-account-tests.js');  // FIP-6 tests of generic account functionality
-
-  //require('./tests/clio.js');  // FIP-6
-
-  //require('./tests/transfer-address.js'); // FIP-1.b
-
+  //require('./tests/clio.js');  // FIP-16
   
   //require('./tests/locks.js');  // Depends on local wallet. Need to fix
   //require('./tests/testnet-smoketest.js'); // In development
