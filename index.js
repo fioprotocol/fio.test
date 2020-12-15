6de2abf32c8af1b2a784e98fa38240068aa11e87
Eric Butz <ericgbutz@gmail.com>
2020-12-14 20:19:39 -0700

    updates from bravo

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const {expect} = require('chai')
 
 describe('TEST SUITE', () => {
 
+  require('./tests/bravo-migr-test.js'); 
+
   //require('./tests/history.js'); // Only run against history node.
   require('./tests/addaddress.js'); // v1.0.x  Also includes FIP-13 tests.
   require('./tests/fees.js'); // v1.0.x
@@ -21,7 +23,7 @@ describe('TEST SUITE', () => {
   require('./tests/burn-address.js'); // FIP-7
   //require('./tests/fee-voting-fee-setting.js'); // FIP-10
   //require('./tests/producer-fee-setting.js');  // FIP-10
-  require('./tests/record-obt-data.js'); //FIP-1.b testing
+  require('./tests/record-obt-data-old.js'); //FIP-1.b testing
   //require('./tests/transfer-address.js'); // FIP-1.b
   require('./tests/transfer-locked-tokens.js');  // FIP-6 locking tests
   require('./tests/transfer-locked-tokens-account-tests.js');  // FIP-6 tests of generic account functionality
