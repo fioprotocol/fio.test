ab52d8a1103b8e9b7901c26aa0dfb0262c852bf9
Eric Butz <ericgbutz@gmail.com>
2020-12-21 17:45:33 -0700

    Vote.js lowercase fiosdk

--- tests/vote.js
+++ tests/vote.js
@@ -1,7 +1,7 @@
 require('mocha')
 const {expect} = require('chai')
 const {newUser, existingUser, getProdVoteTotal, timeout, unlockWallet, addLock, getAccountVoteWeight, getTotalVotedFio, callFioApi, callFioApiSigned, fetchJson} = require('../utils.js');
-const {FIOSDK } = require('@fioprotocol/FIOSDK')
+const {FIOSDK } = require('@fioprotocol/fiosdk')
 config = require('../config.js');
 
 const eosio = {
