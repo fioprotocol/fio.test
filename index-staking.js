ac9e12eca99a997d87b80a55d0a77f72c993a77a
ben <beale.ben@gmail.com>
2021-11-02 13:31:27 -0400

    Buggy staking test fixes and test maintenance.

--- index-staking.js
+++ index-staking.js
@@ -4,6 +4,8 @@ const {expect} = require('chai')
 describe('TEST SUITE', () => {
 
   require('./tests/locks-mainnet-locked-tokens.js');
+  require('./tests/locks-mainnet-locked-tokens-lock1hotfix');
+  require('./tests/locks-transfer-locked-tokens-testnet-smoke-tests');
   require('./tests/locks-get-locks.js');
   require('./tests/stake-timing.js');
   require('./tests/stake-tokens.js');
