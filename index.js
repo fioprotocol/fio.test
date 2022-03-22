548309b6b3494f23981bbfc9c4d8fc69c0513e2a
ben <beale.ben@gmail.com>
2022-03-22 12:34:04 -0400

    Separate context for wrapping tests.

--- index.js
+++ index.js
@@ -4,13 +4,35 @@ const {expect} = require('chai');
 describe('TEST SUITE', () => {
 
   /**
+   * WFIO and FIONFT wrapping and unwrapping
+   * FIO token wrapping and unwrapping (FIP-17a)
+   * FIO domain wrapping and unwrapping (FIP-17b)
+   */
+  describe.only(`** FIP-17a and b - WRAPPING TESTS **`, function () {
+
+    // require('./tests/fio-erc20');
+    //
+    // require('./tests/fio-erc721');
+
+    require('./tests/fio-token-wrapping-sdk');
+
+    require('./tests/fio-token-wrapping-api');
+
+    require('./tests/fio-domain-wrapping-sdk');
+
+    require('./tests/fio-domain-wrapping-api');
+
+    // require('./tests/fio-wrapping-integration-tests');
+  });
+
+  /**
    * Staking Tests (FIP-21)
    * May require additional configuration (see the notes in js files before running these tests)
    * in addition to this we need to possibly develop more tests for checking voting power when accounts have staked
    *
    * To quickly obtain the local changes in fio.contracts, checkout branch ben/develop in that repository
    */
-  describe.skip('STAKING TESTS', () => {
+  describe.skip('** FIP-21 - STAKING TESTS **', () => {
 
     /**
      * !!! These Staking tests require additional configuration !!!
@@ -69,14 +91,6 @@ describe('TEST SUITE', () => {
    * !!! ERC20 and ERC721 contract tests inside this test suite rely on hardhat
    * Be sure to rerun npm install !!!
    */
-
-  /**
-   * FIP-17 FIO Token Wrapping
-   */
-  require('./tests/fio-erc20.js');
-  require('./tests/fio-token-wrapping-sdk.js');
-  require('./tests/fio-token-wrapping-api.js');
-  //require('./tests/fio-wrapping-integration-tests.js');
 
   /**
    * FIP-27 FIO NFT
