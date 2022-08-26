228ca315cd143b1829c83c68d0afe3eea1cbfaff
Ed Rotthoff <ed@fioprotocol.io>
2022-08-26 09:08:13 -0600

    FIP-39. add tests for updcryptkey
    FIP-39 dev tests for updcryptkey.

--- tests/FIP-39-dev-tests.js
+++ tests/FIP-39-dev-tests.js
@@ -1,6 +1,6 @@
 require('mocha')
 const {expect} = require('chai')
-const {newUser, callFioApi,fetchJson, createKeypair} = require('../utils.js');
+const {newUser, callFioApi,fetchJson, timeout,createKeypair} = require('../utils.js');
 const {FIOSDK } = require('@fioprotocol/fiosdk')
 const config = require('../config.js');
 let faucet;
@@ -13,7 +13,15 @@ before(async () => {
  *
  *no setup required
  *
- * these tests perform the testing described in BD-3552
+ * these tests perform the dev testing described for FIP-39.
+ *
+ * in addition to these tests is is very important to dev test regaddress, xferaddress, and burnaddress
+ * as these are affected
+ *
+ * for regaddress after the reg note an entry appears in the handleinfo table.
+ * for transfer address note that the entry in the handleinfo is modified correctly to contain the new
+ * public key.
+ * for burnaddress verify that all entries in the handleinfo for this fioname id are removed from the table.
  * 
  * 
  */
@@ -35,6 +43,7 @@ describe(`************************** FIP-39-dev-tests.js ***********************
 
   before(async () => {
     userUpdateEncryptKey = await newUser(faucet);
+
   })
 
   it.skip('updcryptkey -- table check todo, use this', async () => {
@@ -66,11 +75,9 @@ describe(`************************** FIP-39-dev-tests.js ***********************
   })
 
 
-
-  //updcryptkey(const string &fio_address, const string &encrypt_public_key, const int64_t &max_fee,
-  //                    const name &actor,
-  //                    const string &tpid) {
-
+  //begin updcryptkey success tests.
+  //for these next three tests, during the 30 second wait, do a get table manually
+  //and verify table contents manually.
   it(`fio.address updcryptkey`, async () => {
     try {
       const result = await userUpdateEncryptKey.sdk.genericAction('pushTransaction', {
@@ -91,5 +98,78 @@ describe(`************************** FIP-39-dev-tests.js ***********************
       expect(err).to.equal('null')
     }
   })
+
+  //wait a bit
+  it(`Waiting for 30 sec`, async () => {
+    console.log("            waiting 30 seconds \n ");
+  });
+
+  it('Wait for 30', async () => {
+    await timeout(30000);
+  });
+
+
+  it(`fio.address updcryptkey`, async () => {
+    try {
+      const result = await userUpdateEncryptKey.sdk.genericAction('pushTransaction', {
+        action: 'updcryptkey',
+        account: 'fio.address',
+        data: {
+          fio_address: userUpdateEncryptKey.address,
+          encrypt_public_key: "FIO6oQ4mkezh9krzMJaGiwaZCHp3UsBHphEFhMBATS7VT4v9aGeLM",
+          max_fee : config.maxFee,
+          actor: userUpdateEncryptKey.account,
+          tpid: ''
+        }
+      })
+      console.log('Result: ', result)
+      expect(result.status).to.equal('OK')
+    } catch (err) {
+      console.log('Error: ', err)
+      expect(err).to.equal('null')
+    }
+  })
+
+  //wait a bit
+  it(`Waiting for 30 sec`, async () => {
+    console.log("            waiting 30 seconds \n ");
+  });
+
+  it('Wait for 30', async () => {
+    await timeout(30000);
+  });
+
+  it(`fio.address updcryptkey`, async () => {
+    try {
+      const result = await userUpdateEncryptKey.sdk.genericAction('pushTransaction', {
+        action: 'updcryptkey',
+        account: 'fio.address',
+        data: {
+          fio_address: userUpdateEncryptKey.address,
+          encrypt_public_key: "FIO5fLPj977LQhbBd2qwuHz6c4io9zTt78bnVNrcCwZLsn3AzJwWv",
+          max_fee : config.maxFee,
+          actor: userUpdateEncryptKey.account,
+          tpid: ''
+        }
+      })
+      console.log('Result: ', result)
+      expect(result.status).to.equal('OK')
+    } catch (err) {
+      console.log('Error: ', err)
+      expect(err).to.equal('null')
+    }
+  })
+  //end updcryptkey success tests.
+
+  //next we will test regaddress, xferaddress, and burnaddress
+      /*
+  * for regaddress after the reg note an entry appears in the handleinfo table.
+  * for transfer address note that the entry in the handleinfo is modified correctly to contain the new
+  * public key.
+  * for burnaddress verify that all entries in the handleinfo for this fioname id are removed from the table.
+  *
+  * */
+
+      
 
 })
