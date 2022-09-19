a0e2a83c57dd33c13bea674157a8bd775f6b2055
Eric Butz <ericgbutz@gmail.com>
2022-09-19 09:09:29 -0600

    Test for bd-4083

--- tests/producer.js
+++ tests/producer.js
@@ -1,6 +1,6 @@
 require('mocha')
 const {expect} = require('chai')
-const {newUser, generateFioAddress, timeout, fetchJson} = require('../utils.js');
+const {newUser, generateFioAddress, callFioApi, timeout, fetchJson} = require('../utils.js');
 const {FIOSDK } = require('@fioprotocol/fiosdk')
 config = require('../config.js');
 
@@ -8,7 +8,7 @@ before(async () => {
   faucet = new FIOSDK(config.FAUCET_PRIV_KEY, config.FAUCET_PUB_KEY, config.BASE_URL, fetchJson)
 })
 
-describe('************************** producer.js ************************** \n    A. Test register as a proxy.', () => {
+describe.only('************************** producer.js ************************** \n    A. Test register/unregister as a producer.', () => {
 
   let prodA1, userA1, total_voted_fio, total_bp_votes
 
@@ -16,22 +16,6 @@ describe('************************** producer.js ************************** \n  
     prodA1 = await newUser(faucet);
     userA1 = await newUser(faucet);
     userA1.address2 = generateFioAddress(userA1.domain, 5)
-/*
-  #bp1:dapix
-  #Private key: 5KQ6f9ZgUtagD3LZ4wcMKhhvK9qy4BuwL3L1pkm6E2v62HCne2R
-  #Public key: FIO7jVQXMNLzSncm7kxwg9gk7XUBYQeJPk8b6QfaK5NVNkh3QZrRr
-  #FIO Public Address (actor name): qbxn5zhw2ypw
-
-  #bp2:dapix
-  #Private key: 5JnhMxfnLhZeRCRvCUsaHbrvPSxaqjkQAgw4ZFodx4xXyhZbC9P
-  #Public key: FIO7uTisye5w2hgrCSE1pJhBKHfqDzhvqDJJ4U3vN9mbYWzataS2b
-  #FIO Public Address (actor name): hfdg2qumuvlc
-
-  #bp3:dapix
-  #Private key: 5JvmPVxPxypQEKPwFZQW4Vx7EC8cDYzorVhSWZvuYVFMccfi5mU
-  #Public key: FIO6oa5UV9ghWgYH9en8Cv8dFcAxnZg2i9z9gKbnHahciuKNRPyHc
-  #FIO Public Address (actor name): wttywsmdmfew
-*/
   })
 
   it(`Register userA1 address #2`, async () => {
@@ -42,7 +26,28 @@ describe('************************** producer.js ************************** \n  
     })
     //console.log('Result: ', result)
     expect(result.status).to.equal('OK')
-  })
+  });
+
+  it(`Confirm prodA1 is not a registered producer`, async function () {
+    try {
+      const json = {
+        "code": "eosio",
+        "scope": "eosio",
+        "table": "producers",
+        "lower_bound": prodA1.account,
+        "upper_bound": prodA1.account,
+        "key_type": "name",
+        "index_position": "4",
+        "json": true
+      }
+      const result = await callFioApi("get_table_rows", json);
+      console.log('result: ', result);
+      expect(result.rows.length).to.equal(0);
+    } catch (err) {
+      console.log('Error', err);
+      expect(err).to.equal(null);
+    }
+  });
 
   it(`Register prodA1 as producer`, async () => {
     try {
@@ -65,7 +70,30 @@ describe('************************** producer.js ************************** \n  
     }
   })
 
-  it(`Wait a few seconds.`, async () => { await timeout(3000) })
+  it(`Wait a few seconds.`, async () => { await timeout(3000) });
+
+  it(`Confirm prodA1 is registered as producer`, async function () {
+    try {
+      const json = {
+        "code": "eosio",
+        "scope": "eosio",
+        "table": "producers",
+        "lower_bound": prodA1.account,
+        "upper_bound": prodA1.account,
+        "key_type": "name",
+        "index_position": "4",
+        "json": true
+      }
+      const result = await callFioApi("get_table_rows", json);
+      console.log('result: ', result);
+      expect(result.rows.length).to.equal(1);
+      expect(result.rows[0].owner).to.equal(prodA1.account);
+      expect(result.rows[0].producer_public_key).to.equal(prodA1.publicKey);
+    } catch (err) {
+      console.log('Error', err);
+      expect(err).to.equal(null);
+    }
+  });
 
   it(`userA1 votes for prodA1 using address #1`, async () => {
     try {
@@ -125,6 +153,142 @@ describe('************************** producer.js ************************** \n  
     } catch (err) {
       console.log('Error: ', err)
     }
-  })
+  });
+
+  it(`Confirm prodA1 is not a registered producer`, async function () {
+    try {
+      const json = {
+        "code": "eosio",
+        "scope": "eosio",
+        "table": "producers",
+        "lower_bound": prodA1.account,
+        "upper_bound": prodA1.account,
+        "key_type": "name",
+        "index_position": "4",
+        "json": true
+      }
+      const result = await callFioApi("get_table_rows", json);
+      console.log('result: ', result);
+      expect(result.rows.length).to.equal(0);
+    } catch (err) {
+      console.log('Error', err);
+      expect(err).to.equal(null);
+    }
+  });
 
 })
+
+describe('B. regproducer with pub key not associated with account (BD-3521)', () => {
+  let user1, user2;
+
+  it(`Create users`, async () => {
+    user1 = await newUser(faucet);
+    user2 = await newUser(faucet);
+  });
+
+  it(`Confirm user1 is not a registered producer`, async function () {
+    try {
+      const json = {
+        "code": "eosio",
+        "scope": "eosio",
+        "table": "producers",
+        "lower_bound": user1.account,
+        "upper_bound": user1.account,
+        "key_type": "name",
+        "index_position": "4",
+        "json": true
+      }
+      const result = await callFioApi("get_table_rows", json);
+      console.log('result: ', result);
+      expect(result.rows.length).to.equal(0);
+    } catch (err) {
+      console.log('Error', err);
+      expect(err).to.equal(null);
+    }
+  });
+
+  it(`Register user1 as producer using user2 public key`, async () => {
+    try {
+      const result = await user1.sdk.genericAction('pushTransaction', {
+        action: 'regproducer',
+        account: 'eosio',
+        data: {
+          fio_address: user1.address,
+          fio_pub_key: user1.publicKey,
+          url: "https://mywebsite.io/",
+          location: 80,
+          actor: user1.account,
+          max_fee: config.maxFee
+        }
+      })
+      //console.log('Result: ', result)
+      expect(result.status).to.equal('OK');
+    } catch (err) {
+      console.log('Error: ', err.json);
+      expect(err).to.equal('null');
+    }
+  });
+
+  it(`Confirm user1 is registered as producer`, async function () {
+    try {
+      const json = {
+        "code": "eosio",
+        "scope": "eosio",
+        "table": "producers",
+        "lower_bound": user1.account,
+        "upper_bound": user1.account,
+        "key_type": "name",
+        "index_position": "4",
+        "json": true
+      }
+      const result = await callFioApi("get_table_rows", json);
+      console.log('result: ', result);
+      expect(result.rows.length).to.equal(1);
+      expect(result.rows[0].owner).to.equal(user1.account);
+      expect(result.rows[0].producer_public_key).to.equal(user2.publicKey);
+    } catch (err) {
+      console.log('Error', err);
+      expect(err).to.equal(null);
+    }
+  });
+
+  it(`Unregister user1 as producer (cleanup)`, async () => {
+    try {
+      const result = await user1.sdk.genericAction('pushTransaction', {
+        action: 'unregprod',
+        account: 'eosio',
+        data: {
+          fio_address: user1.address,
+          max_fee: config.maxFee
+        }
+      })
+      console.log('Result: ', result)
+      expect(result.status).to.equal('OK');
+    } catch (err) {
+      console.log('Error: ', err);
+      expect(err).to.equal(null);
+    }
+  });
+
+  it(`Confirm user1 is unregistered`, async function () {
+    try {
+      const json = {
+        "code": "eosio",
+        "scope": "eosio",
+        "table": "producers",
+        "lower_bound": user1.account,
+        "upper_bound": user1.account,
+        "key_type": "name",
+        "index_position": "4",
+        "json": true
+      }
+      const result = await callFioApi("get_table_rows", json);
+      console.log('result: ', result);
+      expect(result.rows.length).to.equal(0);
+    } catch (err) {
+      console.log('Error', err);
+      expect(err).to.equal(null);
+    }
+  });
+
+});
