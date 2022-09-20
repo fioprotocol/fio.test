c7fb582a41f000b9ebc5a8878e478d72ee9dbcaa
Eric Butz <47221604+ericbutz@users.noreply.github.com>
2022-09-20 15:49:03 -0600

    Eric/bd 4083 (#302)
    * Repro setup for BD-4082
    
    * Test for bd-4083
    
    * Final fixes for bd4083 not a bug

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const TESTURL = 'http://localhost:8889'           // Localhost
+//const TESTURL = 'http://localhost:8889'           // Localhost
 //const TESTURL = 'http://54.200.124.65:8889'       // QAnet
-//const TESTURL = 'http://35.85.153.248:8889'       // DEV1
+const TESTURL = 'http://18.237.87.177:8889'       // DEV1
 //const TESTURL = 'http://44.238.153.162:8889'      // Devnet
 //const TESTURL = 'http://fiotestnet.greymass.com'  // Testnet
 //const TESTURL = 'https://fio.greymass.com'        // Mainnet
