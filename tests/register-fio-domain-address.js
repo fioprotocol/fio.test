752405d2c3631b29ed049aaac06a75a8a1646be2
ben <beale.ben@gmail.com>
2022-10-28 15:58:39 -0400

    Added a few tests using /register_fio_domain_address.

--- tests/register-fio-domain-address.js
+++ tests/register-fio-domain-address.js
@@ -9,7 +9,7 @@ before(async () => {
   faucet = new FIOSDK(config.FAUCET_PRIV_KEY, config.FAUCET_PUB_KEY, config.BASE_URL, fetchJson);
 });
 
-describe(`************************** register-fio-domain-address.js ************************** \n    A. Check valid and invalid domain and address formats`, function () {
+describe(`************************** register-fio-domain-address.js ************************** \n    A. Register a FIO domain and addres in one transaction`, function () {
 
   let user1, user2, bp;
   let domain1 = generateFioDomain(5);
@@ -484,5 +484,173 @@ describe(`************************** register-fio-domain-address.js ************
     } catch (err) {
       expect(err.message).to.equal('missing regdomadd.actor (type=name)');
     }
+  });
+});
+
+describe(`B. Register a FIO domain and address using /register_fio_domain_address`, function () {
+
+  let user1, user2, bp;
+  let domain1 = generateFioDomain(5);
+  let domain2 = generateFioDomain(10);
+  let address1 = generateFioAddress(domain1, 5);
+  let address2 = generateFioAddress(domain2, 9);
+
+  before(async function () {
+    bp = await existingUser('qbxn5zhw2ypw', '5KQ6f9ZgUtagD3LZ4wcMKhhvK9qy4BuwL3L1pkm6E2v62HCne2R', 'FIO7jVQXMNLzSncm7kxwg9gk7XUBYQeJPk8b6QfaK5NVNkh3QZrRr', 'dapixdev', 'bp1@dapixdev');
+    user1 = await newUser(faucet);
+    user2 = await newUser(faucet);
+  });
+
+  it(`register a FIO address and a public FIO domain`, async function () {
+    try {
+      const result = await callFioApiSigned('register_fio_domain_address', {
+        action: 'regdomadd',
+        account: 'fio.address',
+        actor: user1.account,
+        privKey: user1.privateKey,
+        data: {
+          fio_address: address1,
+          is_public: 1,
+          owner_fio_public_key: user1.publicKey,
+          max_fee: config.maxFee,
+          tpid: bp.address,
+          actor: user1.account
+        }
+      });
+      expect(result).to.have.all.keys('transaction_id', 'processed');
+      expect(result.processed.receipt.status).to.equal('executed');
+      expect(result.processed.action_traces[0].receipt.response).to.contain('"status": "OK"').and.contain('"fee_collected":800000000000').and.contain('"expiration":');
+      expect(result.processed.action_traces[0].act.data.fio_address).to.equal(address1);
+      expect(result.processed.action_traces[0].act.data.is_public).to.equal(1);
+      expect(result.processed.action_traces[0].act.data.owner_fio_public_key).to.equal(user1.publicKey);
+    } catch (err) {
+      throw err;
+    }
+  });
+
+  it(`(invalid fio_address) try to register a FIO address and a public FIO domain`, async function () {
+    const result = await callFioApiSigned('register_fio_domain_address', {
+      action: 'regdomadd',
+      account: 'fio.address',
+      actor: user1.account,
+      privKey: user1.privateKey,
+      data: {
+        fio_address: "!$Invalid#",
+        is_public: 1,
+        owner_fio_public_key: user1.publicKey,
+        max_fee: 30000000000,
+        tpid: bp.address,
+        actor: user1.account
+      }
+    });
+    expect(result.type).to.equal('invalid_input');
+    expect(result.fields[0].name).to.equal('fio_address');
+    expect(result.fields[0].value).to.equal('!$invalid#');
+    expect(result.fields[0].error).to.equal('Invalid FIO Address format');
+  });
+
+  it(`(invalid is_public) try to register a FIO address and a public FIO domain`, async function () {
+    try {
+      const result = await callFioApiSigned('register_fio_domain_address', {
+        action: 'regdomadd',
+        account: 'fio.address',
+        actor: user2.account,
+        privKey: user2.privateKey,
+        data: {
+          fio_address: generateFioAddress(generateFioDomain(5), 5),
+          is_public: "!@invalid#$",
+          owner_fio_public_key: user2.publicKey,
+          max_fee: config.maxFee,
+          tpid: bp.address,
+          actor: user2.account
+        }
+      });
+      expect(result).to.equal(null);
+    } catch (err) {
+      expect(err.message).to.equal('Expected number');
+    }
+  });
+
+  it(`(invalid owner_fio_public_key) try to register a FIO address and a public FIO domain`, async function () {
+    const result = await callFioApiSigned('register_fio_domain_address', {
+      action: 'regdomadd',
+      account: 'fio.address',
+      actor: user2.account,
+      privKey: user2.privateKey,
+      data: {
+        fio_address: generateFioAddress(generateFioDomain(5), 5),
+        is_public: 1,
+        owner_fio_public_key: "!@invalid#$",
+        max_fee: config.maxFee,
+        tpid: bp.address,
+        actor: user2.account
+      }
+    });
+    expect(result.type).to.equal('invalid_input');
+    expect(result.fields[0].name).to.equal('owner_fio_public_key');
+    expect(result.fields[0].value).to.equal('!@invalid#$');
+    expect(result.fields[0].error).to.equal('Invalid FIO Public Key format');
+  });
+
+  it(`(invalid max_fee) try to register a FIO address and a public FIO domain`, async function () {
+    try {
+      const result = await callFioApiSigned('register_fio_domain_address', {
+        action: 'regdomadd',
+        account: 'fio.address',
+        actor: user2.account,
+        privKey: user2.privateKey,
+        data: {
+          fio_address: generateFioAddress(generateFioDomain(5), 5),
+          is_public: 1,
+          owner_fio_public_key: user2.publicKey,
+          max_fee: "!@invalid#$",
+          tpid: bp.address,
+          actor: user2.account
+        }
+      });
+      expect(result).to.equal(null);
+    } catch (err) {
+      expect(err.message).to.equal('invalid number');
+    }
+  });
+
+  it(`(invalid tpid) try to register a FIO address and a public FIO domain`, async function () {
+    const result = await callFioApiSigned('register_fio_domain_address', {
+      action: 'regdomadd',
+      account: 'fio.address',
+      actor: user2.account,
+      privKey: user2.privateKey,
+      data: {
+        fio_address: generateFioAddress(generateFioDomain(5), 5),
+        is_public: 1,
+        owner_fio_public_key: user2.publicKey,
+        max_fee: config.maxFee,
+        tpid: "!@invalid#$",
+        actor: user2.account
+      }
+    });
+    expect(result.type).to.equal('invalid_input');
+    expect(result.fields[0].name).to.equal('tpid');
+    expect(result.fields[0].value).to.equal('!@invalid#$');
+    expect(result.fields[0].error).to.equal('TPID must be empty or valid FIO address');
+  });
+
+  it(`(invalid actor) try to register a FIO address and a public FIO domain`, async function () {
+    const result = await callFioApiSigned('register_fio_domain_address', {
+      action: 'regdomadd',
+      account: 'fio.address',
+      actor: user2.account,
+      privKey: user2.privateKey,
+      data: {
+        fio_address: generateFioAddress(generateFioDomain(5), 5),
+        is_public: 1,
+        owner_fio_public_key: user2.publicKey,
+        max_fee: config.maxFee,
+        tpid: bp.address,
+        actor: "!#invalid$%"
+      }
+    });
+    expect(result.error.name).to.equal('missing_auth_exception');
+    expect(result.error.details[0].message).to.equal('missing authority of ..invalid');
   });
 });
