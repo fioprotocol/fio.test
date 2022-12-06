c9741154efb9f6bffabe3b99625b9b415813da20
ben <beale.ben@gmail.com>
2022-12-06 10:07:56 -0500

    FIP-43 tests.

--- tests/fees.js
+++ tests/fees.js
@@ -2,7 +2,9 @@ require('mocha')
 const {expect} = require('chai')
 const {newUser, fetchJson} = require('../utils.js');
 const {FIOSDK } = require('@fioprotocol/fiosdk')
+const {callFioApi, callFioApiSigned} = require("../utils");
 config = require('../config.js');
+let faucet;
 
 before(async () => {
   faucet = new FIOSDK(config.FAUCET_PRIV_KEY, config.FAUCET_PUB_KEY, config.BASE_URL, fetchJson)
@@ -65,6 +67,50 @@ describe('************************** fees.js ************************** \n    Te
       } catch (err) {
         console.log('Error: ', err)
       }
+    }
+  })
+})
+
+describe(`B. do not require FIO addresses for certain fees`, () => {
+  let userB1
+
+  before(async () => {
+    userB1 = await newUser(faucet);
+  })
+
+  it(`call get_fee on stake_fio_tokens without passing in a fio_address, expect OK`, async () => {
+    try {
+      const result = await callFioApi('get_fee', {"end_point": "stake_fio_tokens"})
+      expect(result).to.have.property('fee').which.is.a('number').and.equals(0)
+    } catch (err) {
+      throw err
+    }
+  })
+
+  it(`call get_fee on unstake_fio_tokens without passing in a fio_address, expect OK`, async () => {
+    try {
+      const result = await callFioApi('get_fee', {"end_point": "unstake_fio_tokens"})
+      expect(result).to.have.property('fee').which.is.a('number').and.equals(0)
+    } catch (err) {
+      throw err
+    }
+  })
+
+  it(`call get_fee on vote_producer without passing in a fio_address, expect OK`, async () => {
+    try {
+      const result = await callFioApi('get_fee', {"end_point": "vote_producer"})
+      expect(result).to.have.property('fee').which.is.a('number').and.equals(0)
+    } catch (err) {
+      throw err
+    }
+  })
+
+  it(`call get_fee on proxy_vote without passing in a fio_address, expect OK`, async () => {
+    try {
+      const result = await callFioApi('get_fee', {end_point: "proxy_vote"})
+      expect(result).to.have.property('fee').which.is.a('number').and.equals(0)
+    } catch (err) {
+      throw err
     }
   })
 })
